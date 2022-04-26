c3f6f0ae6b7ebd1c41feb4429ffbbfff25cf3568
unknown <thierbach.jonas.181@gmail.com>
2022-04-26 17:45:38 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2aaedbabd8871e816d59d16ce7855d56.js"
+  "/precache-manifest.4c30fe051e4175a8afcf01f039fa2ee3.js"
 );
 
 self.addEventListener('message', (event) => {
